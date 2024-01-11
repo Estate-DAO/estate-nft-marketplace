5909d2b56f3374ee4e56950eba11e6a6b1f2ed6a
Harsh Es <harsh@estatedao.org>
2024-01-11 09:56:35 +0530

    Add loding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,12 @@
 	<div>About</div>
 </div>
 
-<div class="h-[250px] flex items-center justify-center flex-col space-y-4">
+<div class="h-[400px] flex items-center justify-center flex-col space-y-4 bg-emerald-100">
 	<div class="text-5xl">ESTATE DAO</div>
 	<div class="text-lg">ESTATE DAO</div>
 </div>
 
-<div class="flex items-center justify-between px-8">
+<div class="flex items-center justify-between px-8 pt-16">
 	<div class="text-xl">Explore</div>
 	<button class="bg-emerald-300 rounded-md py-3 px-8">Explore</button>
 </div>
@@ -16,14 +16,16 @@
 <div class="flex py-12 items-center gap-8 justify-center p-8 flex-wrap">
 	{#each new Array(10) as _, i}
 		<div
-			class="rounded-lg shrink-0 shadow-lg flex flex-col overflow-hidden space-y-2 items-center w-72"
+			class="rounded-lg relative shrink-0 shadow-lg flex flex-col overflow-hidden items-center w-72"
 		>
+			<div class="h-72 absolute w-72 z-[1] top-0 bg-black/50 animate-pulse"></div>
+
 			<img
 				alt="thumba"
 				src="https://source.unsplash.com/random?{i}"
-				class="w-full h-72 object-cover"
+				class="w-full h-72 z-[2] object-cover"
 			/>
-			<div class="font-bold">Name of the NFT</div>
+			<div class="font-bold py-2">Name of the NFT</div>
 			<div class="flex divide-x-2 p-4">
 				<div class="flex flex-col items-center justify-center gap-y-2 px-2">
 					<div class="text-md font-thin">Floor</div>
