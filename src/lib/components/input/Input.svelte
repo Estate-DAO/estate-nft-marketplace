6bd3e7374a74a1f8afef479a1045ddf43e8029d9
Harsh Es <harsh@estatedao.org>
2024-02-02 15:57:47 +0530

    Add input component

---
+++ src/lib/components/input/Input.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	export let label: string;
+	export let value: string = '';
+	export let placeholder: string = '';
+</script>
+
+<label class="flex flex-col gap-2">
+	<span class="text-sm font-medium leading-6 text-gray-900">{label}</span>
+	<input
+		{placeholder}
+		bind:value
+		type="text"
+		class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+	/>
+</label>
