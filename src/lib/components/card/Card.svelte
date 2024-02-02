e1b88ad111809287b1c996d24d5da2c1adfaed3f
Harsh Es <harsh@estatedao.org>
2024-02-02 15:57:54 +0530

    Update card size

--- src/lib/components/card/Card.svelte
+++ src/lib/components/card/Card.svelte
@@ -2,14 +2,14 @@
 	export let id: number;
 </script>
 
-<a href="/collection" class="rounded-xl relative overflow-hidden shadow-lg w-96">
+<a href="/collection" class="rounded-xl relative overflow-hidden shadow-lg w-[20rem]">
 	<div class="absolute top-2 left-2 rounded-lg bg-white/90 p-1 px-3 text-black text-xs">
 		Upcoming
 	</div>
 	<img
 		alt="thumba"
 		src="https://source.unsplash.com/random/?house,country,{id}"
-		class="w-full h-72 z-[2] object-cover"
+		class="w-full h-64 z-[2] object-cover"
 	/>
 	<div class="flex flex-col gap-y-1 p-5">
 		<div class="font-bold text-sm">Name of the NFT</div>
