559f22cd1c82342aa0f1f757b1bf4aa6f224b75a
Harsh Es <harsh@estatedao.org>
2024-03-02 17:01:03 +0530

    Fix

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import Button from '$lib/components/button/Button.svelte';
 	import LandingBg from '$lib/components/landing-bg/LandingBg.svelte';
 </script>
 
@@ -6,5 +7,6 @@
 	class="flex flex-col overflow-hidden h-screen w-full items-center justify-center pb-20 gap-4 relative"
 >
 	<div class="text-4xl z-[3] font-thin">Welcome to <span class="font-bold">Estate</span> DAO</div>
+	<Button href="/market" class="z-[3]">Go to marketplace</Button>
 	<LandingBg />
 </div>
