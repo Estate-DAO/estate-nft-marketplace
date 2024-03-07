db6d594d6ebbd44837264d99d59d9dd27544cc86
Harsh Es <harsh@estatedao.org>
2024-03-07 20:30:03 +0530

    Handle error

--- src/routes/(app)/admin/(auth)/new-collection/+page.svelte
+++ src/routes/(app)/admin/(auth)/new-collection/+page.svelte
@@ -13,6 +13,7 @@
 	import { replacer } from '$lib/utils/json';
 	import ImagesInfo from './ImagesInfo.svelte';
 	import { preloadCode } from '$app/navigation';
+	import { load } from '../edit-collection/[id=collectionId]/proxy+page';
 
 	let selectedTab: SelectedTab = 'basic';
 	let loading = false;
@@ -30,97 +31,104 @@
 
 	async function submitForm() {
 		if (loading) return;
-		loading = true;
-		const actor = provisionCanister();
-		res = await actor.init_form_metadata({
-			name: basicInfoData.name,
-			desc: basicInfoData.description,
-			owner: $authState.idString || 'owner',
-			property_images: propertyImages,
-			treasury: basicInfoData.userNNsPrincipal,
-			supply_cap: BigInt(basicInfoData.supplyCap),
-			image_uri: basicInfoData.coverImage,
-			price: BigInt(toE8s(basicInfoData.price)),
-			additional_metadata: [
-				{
-					documents: [],
-					market_details: [
-						{
-							annual_popullation_growth: optional(marketInfoData?.annualPopulationGrowth),
-							average_rent: optional(marketInfoData?.averageRent),
-							median_home_sale_price: optional(marketInfoData?.medianHomeSalePrice),
-							country: optional(marketInfoData?.country),
-							city: optional(marketInfoData?.city),
-							state: optional(marketInfoData?.state),
-							description: optional(marketInfoData?.description),
-							coordinates: optional(marketInfoData?.coordinates)
-						}
-					],
-					property_details: [
-						{
-							affordability: optional(propertyInfoData?.affordability),
-							baths: optional(propertyInfoData?.baths),
-							beds: optional(propertyInfoData?.beds),
-							crime_score: optional(propertyInfoData?.crimeScore),
-							flood_zone: optional(propertyInfoData?.floodZone),
-							last_renovated: optional(propertyInfoData?.lastRenovated),
-							monthly_rent: optional(propertyInfoData?.monthlyRent),
-							occupied: optional(propertyInfoData?.occupied),
-							price_per_sq_foot: optional(propertyInfoData?.priceSqFt),
-							school_score: optional(propertyInfoData?.schoolScore),
-							square_footage: optional(propertyInfoData?.areaSqFoot),
-							year_built: optional(propertyInfoData?.yearBuilt)
-						}
-					],
-					financial_details: [
-						{
-							expense_to_income_ratio: optional(financialInfoData?.overall.expenseToIncomeRatio),
-							total_monthly_cost: optional(financialInfoData?.overall.totalMonthlyCost),
-							investment: [
-								{
-									platform_closing_fee: optional(financialInfoData?.investment.platformClosingFee),
-									initial_maintenance_reserve: optional(
-										financialInfoData?.investment.initialMaintenanceReserve
-									),
-									underlying_asset_price: optional(
-										financialInfoData?.investment.underlyingAssetPrice
-									),
-									min_investment: optional(
-										BigInt(financialInfoData?.investment.mininumInvestment ?? 0)
-									)
-								}
-							],
-							monthly_cash_flow: optional(financialInfoData?.overall.montlyCashFlow),
-							property_insurance: optional(financialInfoData?.overall.propertyInsurance),
-							rents: [
-								{
-									llc_monthly_franchise_tax: optional(
-										financialInfoData?.rents.llcMonthlyFranchiseTax
-									),
-									monthly_utiliiies: optional(financialInfoData?.rents.monthlyUtilities),
-									projected_rent: optional(financialInfoData?.rents.projectedRent),
-									property_managment_fee: optional(financialInfoData?.rents.propertyManagementFee),
-									property_taxes: optional(financialInfoData?.rents.propertyTaxes),
-									vacancy_rate: optional(financialInfoData?.rents.vacancyRate)
-								}
-							],
-							returns: [
-								{
-									average_5_year_roi: optional(financialInfoData?.returns.average5YearROI),
-									cap_rate: optional(financialInfoData?.returns.capRate),
-									projected_appreciation: optional(
-										financialInfoData?.returns.projectedAppreciation
-									),
-									total_5_year_irr: optional(financialInfoData?.returns.total5YearsIrr),
-									yields: optional(financialInfoData?.returns.yields)
-								}
-							]
-						}
-					]
-				}
-			]
-		});
-		loading = false;
+		try {
+			loading = true;
+			const actor = provisionCanister();
+			res = await actor.init_form_metadata({
+				name: basicInfoData.name,
+				desc: basicInfoData.description,
+				owner: $authState.idString || 'owner',
+				property_images: propertyImages,
+				treasury: basicInfoData.userNNsPrincipal,
+				supply_cap: BigInt(basicInfoData.supplyCap),
+				image_uri: basicInfoData.coverImage,
+				price: BigInt(toE8s(basicInfoData.price)),
+				additional_metadata: [
+					{
+						documents: [],
+						market_details: [
+							{
+								annual_popullation_growth: optional(marketInfoData?.annualPopulationGrowth),
+								average_rent: optional(marketInfoData?.averageRent),
+								median_home_sale_price: optional(marketInfoData?.medianHomeSalePrice),
+								country: optional(marketInfoData?.country),
+								city: optional(marketInfoData?.city),
+								state: optional(marketInfoData?.state),
+								description: optional(marketInfoData?.description),
+								coordinates: optional(marketInfoData?.coordinates)
+							}
+						],
+						property_details: [
+							{
+								affordability: optional(propertyInfoData?.affordability),
+								baths: optional(propertyInfoData?.baths),
+								beds: optional(propertyInfoData?.beds),
+								crime_score: optional(propertyInfoData?.crimeScore),
+								flood_zone: optional(propertyInfoData?.floodZone),
+								last_renovated: optional(propertyInfoData?.lastRenovated),
+								monthly_rent: optional(propertyInfoData?.monthlyRent),
+								occupied: optional(propertyInfoData?.occupied),
+								price_per_sq_foot: optional(propertyInfoData?.priceSqFt),
+								school_score: optional(propertyInfoData?.schoolScore),
+								square_footage: optional(propertyInfoData?.areaSqFoot),
+								year_built: optional(propertyInfoData?.yearBuilt)
+							}
+						],
+						financial_details: [
+							{
+								expense_to_income_ratio: optional(financialInfoData?.overall.expenseToIncomeRatio),
+								total_monthly_cost: optional(financialInfoData?.overall.totalMonthlyCost),
+								investment: [
+									{
+										platform_closing_fee: optional(
+											financialInfoData?.investment.platformClosingFee
+										),
+										initial_maintenance_reserve: optional(
+											financialInfoData?.investment.initialMaintenanceReserve
+										),
+										underlying_asset_price: optional(
+											financialInfoData?.investment.underlyingAssetPrice
+										),
+										min_investment: optional(
+											BigInt(financialInfoData?.investment.mininumInvestment ?? 0)
+										)
+									}
+								],
+								monthly_cash_flow: optional(financialInfoData?.overall.montlyCashFlow),
+								property_insurance: optional(financialInfoData?.overall.propertyInsurance),
+								rents: [
+									{
+										llc_monthly_franchise_tax: optional(
+											financialInfoData?.rents.llcMonthlyFranchiseTax
+										),
+										monthly_utiliiies: optional(financialInfoData?.rents.monthlyUtilities),
+										projected_rent: optional(financialInfoData?.rents.projectedRent),
+										property_managment_fee: optional(
+											financialInfoData?.rents.propertyManagementFee
+										),
+										property_taxes: optional(financialInfoData?.rents.propertyTaxes),
+										vacancy_rate: optional(financialInfoData?.rents.vacancyRate)
+									}
+								],
+								returns: [
+									{
+										average_5_year_roi: optional(financialInfoData?.returns.average5YearROI),
+										cap_rate: optional(financialInfoData?.returns.capRate),
+										projected_appreciation: optional(
+											financialInfoData?.returns.projectedAppreciation
+										),
+										total_5_year_irr: optional(financialInfoData?.returns.total5YearsIrr),
+										yields: optional(financialInfoData?.returns.yields)
+									}
+								]
+							}
+						]
+					}
+				]
+			});
+		} finally {
+			loading = false;
+		}
 	}
 </script>
 
