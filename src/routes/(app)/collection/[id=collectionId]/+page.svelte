c5df11cf29966871c242bb8e5949e374f494c589
Harsh Es <harsh@estatedao.org>
2024-03-07 21:29:29 +0530

    Add overflow options

--- src/routes/(app)/collection/[id=collectionId]/+page.svelte
+++ src/routes/(app)/collection/[id=collectionId]/+page.svelte
@@ -18,27 +18,36 @@
 
 	$: metadata = data.metadata;
 	$: waitlist = $page.url.searchParams.has('sample');
+	$: images = (metadata.property_images || []).slice(1, 3).reduce((acc, _c, i, v) => {
+		if (i % 2 === 0) acc.push(v.slice(i, i + 2));
+		return acc;
+	}, []);
 </script>
 
 <div class="w-full flex flex-col items-center gap-4 pb-8">
-	<div class="flex flex-col lg:flex-row gap-2 lg:h-[40rem] w-full overflow-hidden">
+	<div class="flex flex-col lg:flex-row gap-2 lg:h-[40rem] w-full overflow-hidden overflow-x-auto">
 		<img
 			alt="House"
 			src={metadata.property_images[0] || PLACEHOLDER_IMG}
-			class="rounded-xl lg:h-[37rem] lg:w-4/6 object-cover"
+			class="rounded-xl lg:h-[37rem] lg:grow object-cover"
 		/>
-		<div class="flex basis-1/3 flex-col gap-2 lg:h-[37rem]">
-			<img
-				alt="House"
-				src={metadata.property_images[1] || PLACEHOLDER_IMG}
-				class="rounded-xl h-[18.25rem] object-cover"
-			/>
-			<img
-				alt="House"
-				src={metadata.property_images[2] || PLACEHOLDER_IMG}
-				class="rounded-xl h-[18.25rem] object-cover"
-			/>
-		</div>
+
+		{#each images as [im1, im2]}
+			<div class="flex basis-[25%] shrink-0 flex-col gap-2 lg:h-[37rem]">
+				<img
+					alt="House"
+					src={im1 || PLACEHOLDER_IMG}
+					class="rounded-xl h-[18.25rem] object-cover"
+				/>
+				{#if im2}
+					<img
+						alt="House"
+						src={im2 || PLACEHOLDER_IMG}
+						class="rounded-xl h-[18.25rem] object-cover"
+					/>
+				{/if}
+			</div>
+		{/each}
 	</div>
 	<div class="flex flex-col lg:flex-row gap-8 w-full max-w-6xl">
 		<PropertyHeader {metadata} />
