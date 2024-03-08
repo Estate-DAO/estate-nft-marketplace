163795a969515bf6bdbcaafe1572eb4d4b85d801
Harsh Es <harsh@estatedao.org>
2024-03-08 16:31:58 +0530

    Car he

--- src/lib/components/card/Card.svelte
+++ src/lib/components/card/Card.svelte
@@ -4,15 +4,16 @@
 	export let imgSrc = '';
 	export let title = '';
 	export let desc = '';
+	export let status = '';
 	export let sample = false;
 	export let href: string;
 </script>
 
 <a {href} class="rounded-xl relative overflow-hidden shadow-lg w-[20rem]">
 	<div
-		class="absolute top-2 left-2 rounded-lg bg-white/70 backdrop-blur p-1 px-3 text-black text-xs"
+		class="absolute top-2 left-2 rounded-lg bg-white/70 backdrop-blur p-1 px-3 text-black text-xs capitalize"
 	>
-		{sample ? 'Waitlist' : 'Live'}
+		{sample ? 'Waitlist' : status}
 	</div>
 	{#if imgSrc}
 		<img alt="Property preview" src={imgSrc} class="w-full h-64 z-[2] object-cover" />
@@ -22,9 +23,12 @@
 		</div>
 	{/if}
 	<div class="flex flex-col gap-y-1 p-5">
-		<div class="font-bold text-sm">{title}</div>
+		<div class="font-bold text-sm line-clamp-2 h-max">
+			{title}
+			<br /><br />
+		</div>
 		{#if desc}
-			<div class="text-sm line-clamp-2">{desc}</div>
+			<div class="text-sm line-clamp-2 h-max">{desc} <br /><br /></div>
 		{:else}
 			<div class="text-sm">McAllen, TX(USA)</div>
 			<div class="text-sm">3 Beds | 2 Baths | 2,112.00 sq. ft.</div>
