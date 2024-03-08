36e457ea13c644a79ffd1a3345cce2f65cda22bd
Harsh Es <harsh@estatedao.org>
2024-03-08 16:25:25 +0530

    Add bg

--- src/lib/components/landing-bg/LandingBg.svelte
+++ src/lib/components/landing-bg/LandingBg.svelte
@@ -1,26 +1,14 @@
 <script lang="ts">
-	let loadedCount = [0, 0, 0, 0, 0, 0];
+	import bg from '$lib/assets/home-bg.webp';
+	let loaded = false;
 </script>
 
 <div class="absolute inset-0 z-[2] bg-white/60"></div>
-<div class="absolute inset-0 z-[1] grid grid-flow-col-dense grid-rows-11 gap-1">
-	<div class="grid grid-cols-2 md:grid-cols-6 gap-4">
-		{#each new Array(6) as _, i}
-			<div
-				class="grid gap-4 transition-opacity duration-300
-          {loadedCount[i] < 8 ? 'opacity-0' : 'opacity-100'}"
-			>
-				{#each new Array(8) as _, j}
-					<div>
-						<img
-							on:load={() => loadedCount[i]++}
-							class="h-auto max-w-full rounded-lg"
-							src="https://source.unsplash.com/random/?house,country,{i + '' + j}"
-							alt=""
-						/>
-					</div>
-				{/each}
-			</div>
-		{/each}
-	</div>
+<div class="absolute inset-0 h-full z-[1] grid grid-flow-col-dense grid-rows-11 gap-1">
+	<img
+		on:load={() => (loaded = true)}
+		class="h-auto max-w-full rounded-lg {loaded ? 'opacity-100' : 'opacity-5'}"
+		src={bg}
+		alt=""
+	/>
 </div>
