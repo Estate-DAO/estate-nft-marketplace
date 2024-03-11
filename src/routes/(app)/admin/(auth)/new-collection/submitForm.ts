dff8a0a461b9bb2e54d91696b1d3258a6c2573fd
Harsh Es <harsh@estatedao.org>
2024-03-11 09:29:52 +0530

    fix form

--- src/routes/(app)/admin/(auth)/new-collection/submitForm.ts
+++ src/routes/(app)/admin/(auth)/new-collection/submitForm.ts
@@ -8,11 +8,18 @@ import type { FinancialInfoData } from './FinancialInfo.svelte';
 import type { MarketInfoData } from './MarketInfo.svelte';
 import type { PropertyInfoData } from './PropertyInfo.svelte';
 
-export function optional<T>(val: T | undefined | null, string = false): [] | [T] {
+export function optional<T>(val: T, type: 'string' | 'number' | 'bigint' | 'boolean'): [] | [T] {
 	if (val === undefined || val === null) return [];
-	if (typeof val === 'boolean') return [val];
-	if (string) return val ? [val] : [];
-	else return [typeof val === 'bigint' ? (BigInt(val) as T) : (Number(val) as T)];
+	switch (type) {
+		case 'string':
+			return val ? [val] : [];
+		case 'bigint':
+			return [BigInt(Number(val)) as T];
+		case 'number':
+			return [Number(val) as T];
+		default:
+			return [val];
+	}
 }
 
 export function getFormData(
@@ -40,71 +47,108 @@ export function getFormData(
 				documents: [],
 				market_details: [
 					{
-						annual_popullation_growth: optional(marketInfoData?.annualPopulationGrowth),
-						average_rent: optional(marketInfoData?.averageRent),
-						median_home_sale_price: optional(marketInfoData?.medianHomeSalePrice),
-						country: optional(marketInfoData?.country, true),
-						city: optional(marketInfoData?.city, true),
-						state: optional(marketInfoData?.state, true),
-						description: optional(marketInfoData?.description, true),
-						coordinates: optional(marketInfoData?.coordinates, true)
+						annual_popullation_growth: optional<number>(
+							marketInfoData?.annualPopulationGrowth,
+							'number'
+						),
+						average_rent: optional<number>(marketInfoData?.averageRent, 'number'),
+						median_home_sale_price: optional<number>(marketInfoData?.medianHomeSalePrice, 'number'),
+						country: optional<string>(marketInfoData?.country, 'string'),
+						city: optional<string>(marketInfoData?.city, 'string'),
+						state: optional<string>(marketInfoData?.state, 'string'),
+						description: optional<string>(marketInfoData?.description, 'string'),
+						coordinates: optional<string>(marketInfoData?.coordinates, 'string')
 					}
 				],
 				property_details: [
 					{
-						affordability: optional(propertyInfoData?.affordability),
-						baths: optional(propertyInfoData?.baths),
-						beds: optional(propertyInfoData?.beds),
-						crime_score: optional(propertyInfoData?.crimeScore),
-						flood_zone: optional(propertyInfoData?.floodZone),
-						last_renovated: optional(propertyInfoData?.lastRenovated),
-						monthly_rent: optional(propertyInfoData?.monthlyRent),
-						occupied: optional(propertyInfoData?.occupied),
-						price_per_sq_foot: optional(propertyInfoData?.priceSqFt),
-						school_score: optional(propertyInfoData?.schoolScore),
-						square_footage: optional(propertyInfoData?.areaSqFoot),
-						year_built: optional(propertyInfoData?.yearBuilt)
+						affordability: optional<number>(propertyInfoData?.affordability, 'string'),
+						baths: optional<number>(propertyInfoData?.baths, 'string'),
+						beds: optional<number>(propertyInfoData?.beds, 'string'),
+						crime_score: optional<number>(propertyInfoData?.crimeScore, 'string'),
+						flood_zone: optional<boolean>(propertyInfoData?.floodZone, 'string'),
+						last_renovated: optional<number>(propertyInfoData?.lastRenovated, 'string'),
+						monthly_rent: optional<number>(propertyInfoData?.monthlyRent, 'string'),
+						occupied: optional<boolean>(propertyInfoData?.occupied, 'string'),
+						price_per_sq_foot: optional<number>(propertyInfoData?.priceSqFt, 'string'),
+						school_score: optional<number>(propertyInfoData?.schoolScore, 'string'),
+						square_footage: optional<number>(propertyInfoData?.areaSqFoot, 'string'),
+						year_built: optional<number>(propertyInfoData?.yearBuilt, 'string')
 					}
 				],
 				financial_details: [
 					{
-						expense_to_income_ratio: optional(financialInfoData?.overall.expenseToIncomeRatio),
-						total_monthly_cost: optional(financialInfoData?.overall.totalMonthlyCost),
+						expense_to_income_ratio: optional<number>(
+							financialInfoData?.overall.expenseToIncomeRatio,
+							'number'
+						),
+						total_monthly_cost: optional<number>(
+							financialInfoData?.overall.totalMonthlyCost,
+							'number'
+						),
 						investment: [
 							{
-								platform_closing_fee: optional(financialInfoData?.investment.platformClosingFee),
-								initial_maintenance_reserve: optional(
-									financialInfoData?.investment.initialMaintenanceReserve
+								platform_closing_fee: optional<number>(
+									financialInfoData?.investment.platformClosingFee,
+									'number'
 								),
-								underlying_asset_price: optional(
-									financialInfoData?.investment.underlyingAssetPrice
+								initial_maintenance_reserve: optional<number>(
+									financialInfoData?.investment.initialMaintenanceReserve,
+									'number'
 								),
-								min_investment: optional(
-									BigInt(financialInfoData?.investment.mininumInvestment ?? 0)
+								underlying_asset_price: optional<number>(
+									financialInfoData?.investment.underlyingAssetPrice,
+									'number'
+								),
+								min_investment: optional<bigint>(
+									BigInt(financialInfoData?.investment.mininumInvestment ?? 0),
+									'bigint'
 								)
 							}
 						],
-						monthly_cash_flow: optional(financialInfoData?.overall.montlyCashFlow),
-						property_insurance: optional(financialInfoData?.overall.propertyInsurance),
+						monthly_cash_flow: optional<number>(
+							financialInfoData?.overall.montlyCashFlow,
+							'number'
+						),
+						property_insurance: optional<number>(
+							financialInfoData?.overall.propertyInsurance,
+							'number'
+						),
 						rents: [
 							{
-								llc_monthly_franchise_tax: optional(
-									financialInfoData?.rents.llcMonthlyFranchiseTax
+								llc_monthly_franchise_tax: optional<number>(
+									financialInfoData?.rents.llcMonthlyFranchiseTax,
+									'number'
 								),
-								monthly_utiliiies: optional(financialInfoData?.rents.monthlyUtilities),
-								projected_rent: optional(financialInfoData?.rents.projectedRent),
-								property_managment_fee: optional(financialInfoData?.rents.propertyManagementFee),
-								property_taxes: optional(financialInfoData?.rents.propertyTaxes),
-								vacancy_rate: optional(financialInfoData?.rents.vacancyRate)
+								monthly_utiliiies: optional<number>(
+									financialInfoData?.rents.monthlyUtilities,
+									'number'
+								),
+								projected_rent: optional<number>(financialInfoData?.rents.projectedRent, 'number'),
+								property_managment_fee: optional<number>(
+									financialInfoData?.rents.propertyManagementFee,
+									'number'
+								),
+								property_taxes: optional<number>(financialInfoData?.rents.propertyTaxes, 'number'),
+								vacancy_rate: optional<number>(financialInfoData?.rents.vacancyRate, 'number')
 							}
 						],
 						returns: [
 							{
-								average_5_year_roi: optional(financialInfoData?.returns.average5YearROI),
-								cap_rate: optional(financialInfoData?.returns.capRate),
-								projected_appreciation: optional(financialInfoData?.returns.projectedAppreciation),
-								total_5_year_irr: optional(financialInfoData?.returns.total5YearsIrr),
-								yields: optional(financialInfoData?.returns.yields)
+								average_5_year_roi: optional<number>(
+									financialInfoData?.returns.average5YearROI,
+									'number'
+								),
+								cap_rate: optional<number>(financialInfoData?.returns.capRate, 'number'),
+								projected_appreciation: optional<number>(
+									financialInfoData?.returns.projectedAppreciation,
+									'number'
+								),
+								total_5_year_irr: optional<number>(
+									financialInfoData?.returns.total5YearsIrr,
+									'number'
+								),
+								yields: optional<number>(financialInfoData?.returns.yields, 'number')
 							}
 						]
 					}
